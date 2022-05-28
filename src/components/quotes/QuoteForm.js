5935e15db22948994ae80c6f55c551e823ba7d9d
Marc Medhat <marcmedhat6211@gmail.com>
2022-05-28 18:08:16 +0200

    preventing unwanted transitions

--- src/components/quotes/QuoteForm.js
+++ src/components/quotes/QuoteForm.js
@@ -1,4 +1,6 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
+import { Prompt } from "react-router-dom";
+import { Fragment } from "react/cjs/react.production.min";
 
 import Card from "../UI/Card";
 import LoadingSpinner from "../UI/LoadingSpinner";
@@ -7,6 +9,7 @@ import classes from "./QuoteForm.module.css";
 const QuoteForm = (props) => {
   const authorInputRef = useRef();
   const textInputRef = useRef();
+  const [isEntering, setIsEntering] = useState(false);
 
   function submitFormHandler(event) {
     event.preventDefault();
@@ -19,28 +22,56 @@ const QuoteForm = (props) => {
     props.onAddQuote({ author: enteredAuthor, text: enteredText });
   }
 
-  return (
-    <Card>
-      <form className={classes.form} onSubmit={submitFormHandler}>
-        {props.isLoading && (
-          <div className={classes.loading}>
-            <LoadingSpinner />
-          </div>
-        )}
+  const finishEnteringHandler = () => {
+    setIsEntering(false);
+  };
 
-        <div className={classes.control}>
-          <label htmlFor="author">Author</label>
-          <input type="text" id="author" ref={authorInputRef} />
-        </div>
-        <div className={classes.control}>
-          <label htmlFor="text">Text</label>
-          <textarea id="text" rows="5" ref={textInputRef}></textarea>
-        </div>
-        <div className={classes.actions}>
-          <button className="btn">Add Quote</button>
-        </div>
-      </form>
-    </Card>
+  const formFocusedHandler = () => {
+    setIsEntering(true);
+  };
+
+  /**
+   * The Prompt component is used to promp some message in certain cases like for example this case, if a user accidentely tries to leave the page after entering some form data
+   * Prompt component takes 2 props:
+   *  when -> takes a boolean, if it's true, the prompt will then show
+   *  message -> takes a method that returns the message string, why a method ? because this method can for example take the location (current location) as its parameter in case you wanted to include anything about this location in your message
+   */
+  return (
+    <Fragment>
+      <Prompt
+        when={isEntering}
+        message={(location) =>
+          "Are you sure you want to leave ? All your entered data will be lost!"
+        }
+      />
+      <Card>
+        <form
+          className={classes.form}
+          onSubmit={submitFormHandler}
+          onFocus={formFocusedHandler}
+        >
+          {props.isLoading && (
+            <div className={classes.loading}>
+              <LoadingSpinner />
+            </div>
+          )}
+
+          <div className={classes.control}>
+            <label htmlFor="author">Author</label>
+            <input type="text" id="author" ref={authorInputRef} />
+          </div>
+          <div className={classes.control}>
+            <label htmlFor="text">Text</label>
+            <textarea id="text" rows="5" ref={textInputRef}></textarea>
+          </div>
+          <div className={classes.actions}>
+            <button className="btn" onClick={finishEnteringHandler}>
+              Add Quote
+            </button>
+          </div>
+        </form>
+      </Card>
+    </Fragment>
   );
 };
 
--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,8 +1,22 @@
+import { useHistory } from "react-router-dom";
+
 import QuoteForm from "../components/quotes/QuoteForm";
 
 const NewQuote = () => {
+  const history = useHistory(); // the useHistory hook gets back an object with the browser history
+
   const addQuoteHandler = (quoteData) => {
     console.log(quoteData);
+
+    /**
+     * The push and the replace method, both allows us to navigate to another route
+     * The difference between them is:
+     *  push:
+     *    allows the user to go back after being redirected, because simply it adds a new route the history stack
+     *  replace:
+     *    doesn't allow the user to go back because it replaces the current route in the browser history
+     */
+    history.push("/quotes");
   };
 
   return <QuoteForm onAddQuote={addQuoteHandler} />;
