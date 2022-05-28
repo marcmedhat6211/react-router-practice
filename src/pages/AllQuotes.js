0e44a03902d481578e7398041f0ae5d3ac318506
Marc Medhat <marcmedhat6211@gmail.com>
2022-05-28 16:14:00 +0200

    adding the layout component and adding liks to navigate through the app

---
+++ src/pages/AllQuotes.js
@@ -0,0 +1,5 @@
+const AllQuotes = () => {
+  return <h1>Quotes List Page</h1>;
+};
+
+export default AllQuotes;
---
+++ src/pages/NewQuote.js
@@ -0,0 +1,5 @@
+const NewQuote = () => {
+  return <h1>Add Quote Page</h1>;
+};
+
+export default NewQuote;
