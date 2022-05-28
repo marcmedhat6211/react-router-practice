edad92f5e3178674d0fbe69f95730d37583b4a34
Marc Medhat <marcmedhat6211@gmail.com>
2022-05-28 16:24:11 +0200

    adding a list of dummy data

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,5 +1,15 @@
+import { Fragment } from "react";
+import QuoteList from "../components/quotes/QuoteList";
+
+const DUMMY_QUOTES = [
+  { id: "q1", author: "Marc", text: "Learning React is fun!" },
+  { id: "q2", author: "Ahmed", text: "Learning Angular is fun!" },
+  { id: "q3", author: "Khaled", text: "Learning PHP is fun!" },
+  { id: "q4", author: "Mina", text: "Learning Python is fun!" },
+];
+
 const AllQuotes = () => {
-  return <h1>Quotes List Page</h1>;
+  return <QuoteList quotes={DUMMY_QUOTES} />;
 };
 
 export default AllQuotes;
